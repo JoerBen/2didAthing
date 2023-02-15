5756203ff7f2b00623c03fafd9892ed25add5d4d
Jorik Chase <jorikchase@gmail.com>
2023-02-15 11:57:47 +0100

    carousel import

--- scenes/magazine/app.js
+++ scenes/magazine/app.js
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 import fragment from "./fragment.glsl";
 import vertex from "./vertex.glsl";
